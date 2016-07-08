5856913c6f7a4b6c139801b6458408fef9f9178c
Juliana Potengy <juliana.potengy@NAVE035-DVDHILK.nave.local>
2016-07-08 11:03:57 -0300

    raizQuadrada
    Calcular a raiz quadrada de um número diminuindo números ímpares

---
+++ raizQuadrada.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número para descobrir sua raiz quadrada aproximada");
+            int nR = int.Parse(Console.ReadLine());
+            int count = 0;
+
+            for (int i = 1; i <= nR; i+=2)
+            {
+                nR -= i;
+                count++;
+            }
+
+            Console.WriteLine("A raiz quadrada aproximada do número digitado é " + count);
+            Console.ReadLine();
+        }
+    }
+}
